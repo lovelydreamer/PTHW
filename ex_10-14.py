a5cd1ad3322a30b3ae7bf4b091000cc532aa5a44
Christina Mitchell <cmitchell@indeed.com>
2017-06-23 10:11:50 -0500

    adding notes for todays lesson

---
+++ ex_10-14.py
@@ -0,0 +1,76 @@
+
+'''
+Review:
+Read each other's code up to today.
+    Do you understand the comments?
+
+'''
+
+
+# exercise 10:
+    '''
+        What does \ do?
+        What does \n do?
+        Tell me what happens here:
+             "I "understand" joe."
+        what about \t ?
+        Combine %r with double-quote and single-quote escapes and print them out. 
+        Compare %r with %s. 
+        Notice how %r prints it the way you'd write it in your file, but %s prints it the way you'd like to see it?
+    
+    '''
+
+
+# exercise 11:
+    '''
+        What does raw_input() do? How does this differ from %r ?
+        
+        Why is this snippet set up like this: '6\'2"' ?
+    '''
+
+# exercise 12:
+    '''
+        What is a prompt?
+        How do I make one?
+        In Terminal where you normally run python to run your scripts, type pydoc raw_input. What did you see?
+        Use pydoc to also read about open, file, os, and sys.
+        What does %r vs %s do? How are they different? How are they similar?
+    '''
+
+# exercise 13:
+    '''
+        What's an import?
+        Can you explain what's happening below?
+        What's a module?
+        Show me the script in the study drill. How do you think it's working?
+    '''
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+
+
+# exercise 14:
+    '''
+        How is the multiline comment and format operator working together?
+    
+    '''
+
+
+
+
+
+
+'''
+Next weeks' reading:
+
+What do you think of this article? What applications does modulo have?
+https://betterexplained.com/articles/fun-with-modular-arithmetic/
+
+'''
---
+++ ex_15-19.py
@@ -0,0 +1,37 @@
+'''
+Review:
+    What did you think of the modulo article?
+        What applications does modulo have?
+        https://betterexplained.com/articles/fun-with-modular-arithmetic/
+
+
+
+
+
+
+
+'''
+
+
+
+
+# exercise 15:
+'''
+    
+
+
+https://learnpythonthehardway.org/book/ex15.html
+    
+'''
+
+
+
+
+
+'''
+Next weeks' review/reading:
+
+What do you think of this article? What applications does modulo have?
+https://betterexplained.com/articles/fun-with-modular-arithmetic/
+
+'''
