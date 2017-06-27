9d6882af83ae519934af43da42154fcc63d1b1b3
Christina Mitchell <cmitchell@indeed.com>
2017-06-27 16:14:12 -0500

    another week of questions

--- ex_15-19.py
+++ ex_15-19.py
@@ -4,24 +4,53 @@ Review:
         What applications does modulo have?
         https://betterexplained.com/articles/fun-with-modular-arithmetic/
 
-
-
-
-
-
-
 '''
-
-
 
 
 # exercise 15:
+
 '''
-    
 
-
+What questions do you have on the material?
+Show me your comments in the code
 https://learnpythonthehardway.org/book/ex15.html
-    
+
+'''
+
+
+# exercise 16:
+'''
+Let's do that again, can you walk me through this editor script?
+Can you walk me through your similar script?
+'''
+
+# exercise 17:
+'''
+How short did you make the script?   
+What is a string, and what's an int?
+why did you need to run out_file.close()?
+were you able to get import to work?
+how did you play with len()
+'''
+
+# exercise 18:
+'''
+what does a function do?
+How do you make one?
+Let's debug if you weren't able to. (don't worry, it's ok to have trouble!)
+
+see study drills for suggestions:
+https://learnpythonthehardway.org/book/ex18.html
+
+Are you bored yet?
+'''
+
+# exercise 19:
+'''
+Let's see your creative ways to run a function!
+what's a variable?
+what's a function?
+what's an argument?
 '''
 
 
@@ -31,7 +60,6 @@ https://learnpythonthehardway.org/book/ex15.html
 '''
 Next weeks' review/reading:
 
-What do you think of this article? What applications does modulo have?
-https://betterexplained.com/articles/fun-with-modular-arithmetic/
+https://learnpythonthehardway.org/book/ex20.html
 
 '''
